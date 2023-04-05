3677e6dfd546406b9ee3868241c2fedbe2583184
Rob Bridges <Rmb101187@gmail.com>
2023-04-05 16:38:26 -0700

    Wiring up postgress model with user service

---
+++ models/postgress.go
@@ -0,0 +1,44 @@
+package models
+
+import (
+	"database/sql"
+	"fmt"
+	_ "github.com/jackc/pgx/v4/stdlib"
+)
+
+// Open will open a sql connection with the provided Postgres. Callers will need to ensure it's closed
+func Open(config PostgressConfig) (*sql.DB, error) {
+	db, err := sql.Open(
+		"pgx",
+		config.String(),
+	)
+	if err != nil {
+		return nil, fmt.Errorf("error Opening DB: %w", err)
+	}
+	return db, nil
+}
+
+func DefaultPostgresConfig() PostgressConfig {
+	return PostgressConfig{
+		Host:     "localhost",
+		Port:     "5431",
+		User:     "rob",
+		Password: "redtruck",
+		Database: "lenslocked",
+		SSLMODE:  "disable",
+	}
+}
+
+type PostgressConfig struct {
+	Host     string
+	Port     string
+	User     string
+	Password string
+	Database string
+	SSLMODE  string
+}
+
+func (cfg PostgressConfig) String() string {
+	return fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=%s",
+		cfg.Host, cfg.Port, cfg.User, cfg.Password, cfg.Database, cfg.SSLMODE)
+}
