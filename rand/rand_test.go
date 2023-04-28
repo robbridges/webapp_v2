8142c1f56b4c0648f36a664cf9ae7a33c6cde410
Rob Bridges <Rmb101187@gmail.com>
2023-04-28 12:25:44 -0700

    starting a bit of testing, need to implementing logging

--- rand/rand_test.go
+++ rand/rand_test.go
@@ -2,6 +2,7 @@ package rand
 
 import (
 	"crypto/rand"
+	"encoding/base64"
 	"errors"
 	"testing"
 )
@@ -10,6 +11,11 @@ type errorReader struct{}
 
 func (r *errorReader) Read(p []byte) (int, error) {
 	return 0, errors.New("mocked error")
+}
+
+func isString(v interface{}) bool {
+	_, ok := v.(string)
+	return ok
 }
 
 func TestBytes_Error(t *testing.T) {
@@ -36,4 +42,17 @@ func TestBytes_Error(t *testing.T) {
 	if err.Error() != expectedError {
 		t.Errorf("Bytes returned unexpected error message. expected=%q, actual=%q", expectedError, err.Error())
 	}
+}
+
+func TestString(t *testing.T) {
+	t.Run("happy path", func(t *testing.T) {
+		b, err := Bytes(5)
+		if err != nil {
+			t.Errorf("Bytes slice not created")
+		}
+		string := base64.URLEncoding.EncodeToString(b)
+		if !isString(string) {
+			t.Errorf("Byte slice not correctly encoded into string")
+		}
+	})
 }
