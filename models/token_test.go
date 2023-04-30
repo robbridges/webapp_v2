e9a9b6880ed9f0e7a6dd275f66ee3a7e1d9ce009
Rob Bridges <Rmb101187@gmail.com>
2023-04-30 10:31:56 -0700

    Cleaning up token testing a bit

--- models/token_test.go
+++ models/token_test.go
@@ -2,17 +2,15 @@ package models
 
 import (
 	"encoding/base64"
+	"fmt"
 	"testing"
 )
 
 func TestTokenManager_New(t *testing.T) {
 	t.Run("Proper amount of bytes", func(t *testing.T) {
-		tm := &tokenManager{
-			BytesPerToken: MinBytesPerToken,
-		}
+		tm := setTokenManager(32)
 
-		tokenStr, err := tm.New()
-		decodedStr, err := base64.URLEncoding.DecodeString(tokenStr)
+		decodedStr, err := decodeStr(tm)
 
 		if err != nil {
 			t.Errorf("Fail: error creating Token: %s", err.Error())
@@ -28,16 +26,12 @@ func TestTokenManager_New(t *testing.T) {
 	})
 
 	t.Run("Improper amount of bytes", func(t *testing.T) {
-		tm := &tokenManager{
-			BytesPerToken: 6,
-		}
+		tm := setTokenManager(6)
 
-		tokenStr, err := tm.New()
-
-		decodedStr, err := base64.URLEncoding.DecodeString(tokenStr)
+		decodedStr, err := decodeStr(tm)
 
 		if err != nil {
-			t.Errorf("Fail: error creating Token: %s", err.Error())
+			t.Errorf("error decoding str %s", err.Error())
 		}
 
 		want := MinBytesPerToken
@@ -48,4 +42,39 @@ func TestTokenManager_New(t *testing.T) {
 			t.Errorf("Got %d: wanted %d", got, want)
 		}
 	})
+
+	t.Run("User may set bytes above the minimum", func(t *testing.T) {
+		tm := setTokenManager(72)
+		decodedStr, err := decodeStr(tm)
+		if err != nil {
+			t.Errorf("error decoding str %s", err.Error())
+		}
+
+		want := tm.BytesPerToken
+		got := len(decodedStr)
+
+		if got != want {
+			t.Errorf("Got %d: wanted %d", got, want)
+		}
+	})
+}
+
+func setTokenManager(bytes int) *tokenManager {
+	tm := &tokenManager{
+		BytesPerToken: bytes,
+	}
+	return tm
+}
+
+func decodeStr(tm *tokenManager) (string, error) {
+	tokenStr, err := tm.New()
+	if err != nil {
+		return "", fmt.Errorf("new Error: %w", err)
+	}
+	decodedStr, err := base64.URLEncoding.DecodeString(tokenStr)
+	if err != nil {
+		return "", fmt.Errorf("decode tokenstring error : %w", err)
+	}
+
+	return string(decodedStr), nil
 }
