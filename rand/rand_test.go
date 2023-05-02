c4ceadb2794664bce96328dac301c3c300a8e408
Rob Bridges <Rmb101187@gmail.com>
2023-05-01 19:04:59 -0700

    Rand test is good for now, few edge case errors that need an interface implementation

--- rand/rand_test.go
+++ rand/rand_test.go
@@ -1,6 +1,7 @@
 package rand
 
 import (
+	"bytes"
 	"crypto/rand"
 	"encoding/base64"
 	"errors"
@@ -14,47 +15,78 @@ func (r *errorReader) Read(p []byte) (int, error) {
 	return 0, errors.New("mocked error")
 }
 
+type mockReader struct{}
+
+func (m *mockReader) Read(p []byte) (int, error) {
+	// simulate the behavior of rand.Read()
+	copy(p, []byte{0x01, 0x02, 0x03, 0x04, 0x05})
+	return len(p), nil
+}
+
 func isString(v interface{}) bool {
 	_, ok := v.(string)
 	return ok
 }
 
-func TestBytes_Error(t *testing.T) {
+func TestBytes(t *testing.T) {
+	t.Run("Happy path", func(t *testing.T) {
+		byteNumber := 6
+		b, err := Bytes(byteNumber)
+		if err != nil {
+			t.Errorf("Error creating byte slice")
+		}
+		if len(b) != byteNumber {
+			t.Errorf("Not enough bytes were created")
+		}
+	})
+	t.Run("Sad path, read error", func(t *testing.T) {
+		randReader := rand.Reader
 
-	randReader := rand.Reader
+		defer func() {
+			rand.Reader = randReader
+		}()
 
-	defer func() {
-		rand.Reader = randReader
-	}()
+		// replace rand.Reader with an errorReader
+		rand.Reader = &errorReader{}
 
-	// replace rand.Reader with an errorReader
-	rand.Reader = &errorReader{}
+		b, err := Bytes(10)
 
-	b, err := Bytes(10)
+		if b != nil {
+			t.Errorf("Bytes should return nil slice on error")
+		}
 
-	if b != nil {
-		t.Errorf("Bytes should return nil slice on error")
-	}
+		if err == nil {
+			t.Errorf("Bytes should return an error")
+		}
 
-	if err == nil {
-		t.Errorf("Bytes should return an error")
-	}
-
-	expectedError := "bytes: mocked error"
-	if err.Error() != expectedError {
-		t.Errorf("Bytes returned unexpected error message. expected=%q, actual=%q", expectedError, err.Error())
-	}
+		expectedError := "bytes: mocked error"
+		if err.Error() != expectedError {
+			t.Errorf("Bytes returned unexpected error message. expected=%q, actual=%q", expectedError, err.Error())
+		}
+	})
 }
 
 func TestString(t *testing.T) {
+	bytes := 5
 
-	b, err := Bytes(5)
+	string, err := String(bytes)
 	if err != nil {
-		t.Errorf("Bytes slice not created")
+		t.Errorf("String did not return a string")
 	}
-	string := base64.URLEncoding.EncodeToString(b)
+	decodedStr, err := base64.URLEncoding.DecodeString(string)
+	if len(decodedStr) != bytes {
+		t.Errorf("String not created with enough bytes")
+	}
 	if !isString(string) {
 		t.Errorf("Byte slice not correctly encoded into string")
 	}
 
+}
+
+func TestGenerateRandByteSlice(t *testing.T) {
+	byteSlice1 := GenerateRandByteSlice()
+	byteSlice2 := GenerateRandByteSlice()
+	if bytes.Equal(byteSlice1, byteSlice2) {
+		t.Errorf("The function generated the same byte slice.")
+	}
 }
