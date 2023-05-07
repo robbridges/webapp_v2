c02c02d710f9e57d10c6a4c316ea61f6bd6edb0d
Rob Bridges <Rmb101187@gmail.com>
2023-05-06 20:50:13 -0700

    Wrapping up simple controller mocks

--- controllers/users_test.go
+++ controllers/users_test.go
@@ -390,3 +390,228 @@ func TestUsers_CurrentUser(t *testing.T) {
 		}
 	})
 }
+
+func TestUsers_Create(t *testing.T) {
+	mockLogger := &models.MockLogger{}
+	mockUserService := &models.MockUserService{}
+	mockSessionService := &models.MockSessionService{}
+
+	users := Users{
+		Templates: struct {
+			New         Template
+			SignIn      Template
+			CurrentUser Template
+		}{},
+		UserService:    mockUserService,
+		SessionService: mockSessionService,
+	}
+
+	t.Run("happy path", func(t *testing.T) {
+		w := httptest.NewRecorder()
+		r, _ := http.NewRequest("POST", "/create", nil)
+
+		// Wrap the handler with the logger middleware
+		handler := models.LoggerMiddleware(mockLogger)(http.HandlerFunc(users.Create))
+
+		// Add logger to the request context
+		ctx := context.WithValue(r.Context(), "logger", mockLogger)
+		r = r.WithContext(ctx)
+
+		// Set form values
+		data := url.Values{}
+		data.Set("email", "test@test.com")
+		data.Set("password", "secure")
+		r.PostForm = data
+
+		// Mock user service Create method
+		user := &models.User{
+			ID:    1,
+			Email: "test@test.com",
+		}
+		mockUserService.CreateFunc = func(email string, password string) (*models.User, error) {
+			return user, nil
+		}
+
+		// Mock session service Create method
+		session := &models.Session{
+			UserID: user.ID,
+			Token:  "test_session_token",
+		}
+		mockSessionService.CreateFunc = func(userID int) (*models.Session, error) {
+			return session, nil
+		}
+
+		// Call the wrapped handler
+		handler.ServeHTTP(w, r)
+
+		// Assert response
+		if w.Code != http.StatusFound {
+			t.Errorf("unexpected status code: got %v, want %v", w.Code, http.StatusFound)
+		}
+
+		// Assert session cookie was set
+		cookies := w.Result().Cookies()
+		if len(cookies) != 1 {
+			t.Errorf("unexpected number of cookies: got %v, want %v", len(cookies), 1)
+		}
+		if cookies[0].Name != CookieSession {
+			t.Errorf("unexpected cookie name: got %v, want %v", cookies[0].Name, CookieSession)
+		}
+		if cookies[0].Value != session.Token {
+			t.Errorf("unexpected cookie value: got %v, want %v", cookies[0].Value, session.Token)
+		}
+		if cookies[0].Path != "/" {
+			t.Errorf("unexpected cookie path: got %v, want %v", cookies[0].Path, "/")
+		}
+
+		// Assert redirection
+		if w.Header().Get("Location") != "/currentuser" {
+			t.Errorf("unexpected redirect location: got %v, want %v", w.Header().Get("Location"), "/currentuser")
+		}
+
+		if len(mockLogger.ErrorLog) > 0 {
+			t.Errorf("unexpected error logged: %v", mockLogger.ErrorLog[0])
+		}
+	})
+
+	t.Run("create user error", func(t *testing.T) {
+		w := httptest.NewRecorder()
+		r, _ := http.NewRequest("POST", "/signup", nil)
+
+		// Wrap the handler with the logger middleware
+		handler := models.LoggerMiddleware(mockLogger)(http.HandlerFunc(users.Create))
+
+		// Add logger to the request context
+		ctx := context.WithValue(r.Context(), "logger", mockLogger)
+		r = r.WithContext(ctx)
+
+		// Set form values
+		data := url.Values{}
+		data.Set("email", "test@test.com")
+		data.Set("password", "weakpassword")
+		r.PostForm = data
+
+		// Mock UserService to return an error when creating a user
+		mockUserService.CreateFunc = func(email string, password string) (*models.User, error) {
+			return nil, errors.New("create user error")
+		}
+		defer func() {
+			mockUserService.CreateFunc = nil
+		}()
+
+		// Call the wrapped handler
+		handler.ServeHTTP(w, r)
+
+		// Assert response
+		if w.Code != http.StatusInternalServerError {
+			t.Errorf("unexpected status code: got %v, want %v", w.Code, http.StatusInternalServerError)
+		}
+
+		// Assert error was logged
+		if len(mockLogger.ErrorLog) != 1 {
+			t.Fatalf("unexpected number of errors logged: got %v, want 1", len(mockLogger.ErrorLog))
+		}
+		err := mockLogger.ErrorLog[0].Error()
+		expectedErr := "create user error"
+		if !strings.Contains(err, expectedErr) {
+			t.Errorf("unexpected error message: got %v, want %v", err, expectedErr)
+		}
+
+	})
+}
+
+func TestUsers_ProcessSignOut(t *testing.T) {
+	mockLogger := &models.MockLogger{}
+	mockSessionService := &models.MockSessionService{}
+
+	mockSessionService.DeleteSessionFunc = func(token string) error {
+		if token == "valid_token" {
+			return nil
+		} else {
+			return errors.New("session delete error")
+		}
+	}
+
+	users := Users{
+		Templates: struct {
+			New         Template
+			SignIn      Template
+			CurrentUser Template
+		}{},
+		SessionService: mockSessionService,
+	}
+
+	handler := models.LoggerMiddleware(mockLogger)(http.HandlerFunc(users.ProcessSignOut))
+
+	t.Run("happy path", func(t *testing.T) {
+		w := httptest.NewRecorder()
+		r, _ := http.NewRequest("GET", "/signout", nil)
+
+		cookie := &http.Cookie{
+			Name:  CookieSession,
+			Value: "valid_token",
+		}
+		r.AddCookie(cookie)
+
+		ctx := context.WithValue(r.Context(), "logger", mockLogger)
+		r = r.WithContext(ctx)
+
+		handler.ServeHTTP(w, r)
+
+		// Assert response
+		if w.Code != http.StatusFound {
+			t.Errorf("unexpected status code: got %v, want %v", w.Code, http.StatusFound)
+		}
+
+		deleteCookie(w, CookieSession)
+
+		// Assert cookie was deleted
+		cookies := r.Cookies()
+		if len(cookies) != 1 {
+			t.Errorf("unexpected number of cookies: got %d, want 1", len(cookies))
+		} else {
+			if cookies[0].Name != CookieSession {
+				t.Errorf("unexpected cookie name: got %s, want %s", cookies[0].Name, CookieSession)
+			}
+			if cookies[0].MaxAge != 0 {
+				t.Errorf("unexpected cookie max age: got %d, want -1", cookies[0].MaxAge)
+			}
+		}
+
+		if len(mockLogger.ErrorLog) > 0 {
+			t.Errorf("unexpected error logged: %v", mockLogger.ErrorLog[0])
+		}
+	})
+
+	t.Run("sad path", func(t *testing.T) {
+		w := httptest.NewRecorder()
+		r, _ := http.NewRequest("GET", "/signout", nil)
+
+		cookie := &http.Cookie{
+			Name:  CookieSession,
+			Value: "invalid_token",
+		}
+		r.AddCookie(cookie)
+
+		ctx := context.WithValue(r.Context(), "logger", mockLogger)
+		r = r.WithContext(ctx)
+
+		handler.ServeHTTP(w, r)
+
+		if w.Code != http.StatusInternalServerError {
+			t.Errorf("unexpected status code: got %v, want %v", w.Code, http.StatusInternalServerError)
+		}
+
+		// Check if the cookie was deleted
+		cookies := w.Result().Cookies()
+		for _, cookie := range cookies {
+			if cookie.Name == CookieSession {
+				t.Errorf("cookie not deleted: %v", cookie)
+			}
+		}
+
+		if len(mockLogger.ErrorLog) == 0 {
+			t.Error("expected error not logged")
+		}
+	})
+}
