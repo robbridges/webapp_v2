8b0fc326c000604fb510274b5b8f17a475a95c92
Rob Bridges <Rmb101187@gmail.com>
2023-05-24 10:15:53 -0700

    Removed old sql files, all just migrations at this point

--- models/sql/01_users.sql
+++
@@ -1,5 +0,0 @@
-CREATE table users (
-    id SERIAL PRIMARY KEY,
-    email TEXT UNIQUE NOT NULL,
-    password_hash TEXT NOT NULL
-);
--- models/sql/03_logs.sql
+++
@@ -1,6 +0,0 @@
-
-CREATE TABLE logs (
-    id SERIAL PRIMARY KEY,
-    message TEXT  NOT NULL,
-    timestamp TIMESTAMP NOT NULL
-);
