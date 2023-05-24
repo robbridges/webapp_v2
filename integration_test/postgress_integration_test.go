a42601783d89f209f516a2c24005452ea5002f13
Rob Bridges <Rmb101187@gmail.com>
2023-05-23 17:29:21 -0700

    Adding integration unit test for testing migrations

--- integration_test/postgress_integration_test.go
+++ integration_test/postgress_integration_test.go
@@ -1,6 +1,9 @@
 package integration_test
 
 import (
+	"database/sql"
+	"fmt"
+	"github.com/robbridges/webapp_v2/models"
 	"testing"
 	"time"
 )
@@ -24,4 +27,43 @@ func TestOpen(t *testing.T) {
 	if err != nil {
 		t.Errorf("Database timeout: %v", err)
 	}
+}
+
+func TestMigrate(t *testing.T) {
+
+	db, err := setup(t)
+	defer deferDBClose(db, &err)
+	defer teardown()
+
+	err = waitForPing(db, 10*time.Second)
+
+	err = dropTableIfExists(db)
+	if err != nil {
+		t.Fatalf("Failed to drop table: %v", err)
+	}
+
+	err = models.Migrate(db, "../migrations")
+	if err == nil {
+		t.Fatalf("Expected error due to duplicate table: %v", err)
+	}
+
+	tableExists := checkIfTableExists(db, "users")
+	if !tableExists {
+		t.Error("Expected 'users' table to exist after migration, but it doesn't.")
+	}
+}
+
+func dropTableIfExists(db *sql.DB) error {
+	_, err := db.Exec("DROP TABLE IF EXISTS users CASCADE")
+	return err
+}
+
+func checkIfTableExists(db *sql.DB, tableName string) bool {
+	var exists bool
+	query := fmt.Sprintf("SELECT EXISTS (SELECT 1 FROM information_schema.tables WHERE table_name = '%s')", tableName)
+	err := db.QueryRow(query).Scan(&exists)
+	if err != nil {
+		return false
+	}
+	return exists
 }
