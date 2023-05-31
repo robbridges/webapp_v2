38ba0f4f37735f1f5439a07d2f158deb94d67e8c
Rob Bridges <Rmb101187@gmail.com>
2023-05-30 18:59:55 -0700

    Finished testintg of email service

--- models/email_test.go
+++ models/email_test.go
@@ -1,6 +1,11 @@
 package models
 
-import "testing"
+import (
+	"github.com/go-mail/mail/v2"
+	"reflect"
+	"strings"
+	"testing"
+)
 
 func TestNewEmailService(t *testing.T) {
 	mockConfig := SMTPConfig{
@@ -13,5 +18,66 @@ func TestNewEmailService(t *testing.T) {
 	es := NewEmailService(mockConfig)
 	if es.Dialer == nil {
 		t.Errorf("Dialer should not be nil")
+	}
+}
+
+func TestSetFrom(t *testing.T) {
+	tests := []struct {
+		name            string
+		defaultSender   string
+		emailFrom       string
+		expectedResults []string
+	}{
+		{
+			name:            "Email has 'From' field",
+			defaultSender:   "default@example.com",
+			emailFrom:       "test@example.com",
+			expectedResults: []string{"test@example.com"},
+		},
+		{
+			name:            "Email has no 'From' field, default sender is set",
+			defaultSender:   "default@example.com",
+			emailFrom:       "",
+			expectedResults: []string{"default@example.com"},
+		},
+		{
+			name:            "Email has no 'From' field, default sender is not set",
+			defaultSender:   "",
+			emailFrom:       "",
+			expectedResults: []string{"support@webgallery.com"},
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+
+			es := &EmailService{
+				DefaultSender: test.defaultSender,
+			}
+
+			email := Email{
+				From: test.emailFrom,
+			}
+
+			msg := mail.NewMessage()
+
+			es.setFrom(msg, email)
+
+			from := msg.GetHeader("From")
+
+			fromStr := strings.Join(from, ", ")
+
+			matchFound := false
+			for _, expectedResult := range test.expectedResults {
+				if reflect.DeepEqual(fromStr, expectedResult) {
+					matchFound = true
+					break
+				}
+			}
+
+			if !matchFound {
+				t.Errorf("Expected From header to be one of %v, but got %s", test.expectedResults, fromStr)
+			}
+		})
 	}
 }
