e49c1b95b6b8473aa3c06428614d07a24551c7f1
Rob Bridges <Rmb101187@gmail.com>
2023-06-15 18:49:33 -0700

    Gallery create implemented

--- models/gallery.go
+++ models/gallery.go
@@ -1,7 +1,31 @@
 package models
 
+import (
+	"database/sql"
+	"fmt"
+)
+
 type Gallery struct {
 	ID     int
-	USERID int
+	UserID int
 	Title  string
+}
+
+type GalleryService struct {
+	DB *sql.DB
+}
+
+func (svc *GalleryService) Create(title string, userID int) (*Gallery, error) {
+	gallery := Gallery{
+		Title:  title,
+		UserID: userID,
+	}
+	row := svc.DB.QueryRow(`
+		INSERT INTO galleries (title, user_id)
+		VALUES ($1, $2) RETURNING id;`, gallery.Title, gallery.UserID)
+	err := row.Scan(&gallery.ID)
+	if err != nil {
+		return nil, fmt.Errorf("create gallery: %v", err)
+	}
+	return &gallery, nil
 }
