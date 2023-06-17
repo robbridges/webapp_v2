eda3ea3d5cba58ef9db3e7859404d71af1013f34
Rob Bridges <Rmb101187@gmail.com>
2023-06-17 13:50:10 -0700

    Gallery edit controller

--- controllers/galleries.go
+++ controllers/galleries.go
@@ -1,15 +1,19 @@
 package controllers
 
 import (
+	"errors"
 	"fmt"
+	"github.com/go-chi/chi/v5"
 	"github.com/robbridges/webapp_v2/context"
 	"github.com/robbridges/webapp_v2/models"
 	"net/http"
+	"strconv"
 )
 
 type Galleries struct {
 	Templates struct {
-		New Template
+		New  Template
+		Edit Template
 	}
 	GalleryService *models.GalleryService
 }
@@ -42,4 +46,36 @@ func (g Galleries) Create(w http.ResponseWriter, r *http.Request) {
 	//TODO implement this page
 	editPath := fmt.Sprintf("/galleries/%d/edit", gallery.ID)
 	http.Redirect(w, r, editPath, http.StatusFound)
+}
+
+func (g Galleries) Edit(w http.ResponseWriter, r *http.Request) {
+	logger := r.Context().Value("logger").(models.LogInterface)
+	id, err := strconv.Atoi(chi.URLParam(r, "id"))
+	if err != nil {
+		http.Error(w, "Invalid ID", http.StatusFound)
+	}
+
+	gallery, err := g.GalleryService.ByID(id)
+	if err != nil {
+		if errors.Is(err, models.ErrNoData) {
+			http.Error(w, "Gallery not found", http.StatusFound)
+			return
+		}
+		logger.Create(err)
+		http.Error(w, "something went wrong", http.StatusInternalServerError)
+		return
+	}
+	user := context.User(r.Context())
+	if gallery.UserID != user.ID {
+		http.Error(w, "you do not have permission to edit this gallery, only the owner can", http.StatusForbidden)
+	}
+
+	var data struct {
+		ID    int
+		Title string
+	}
+
+	data.ID = gallery.ID
+	data.Title = gallery.Title
+	g.Templates.Edit.Execute(w, r, data)
 }
