5a019e2f53b8b21025b4f968c88abbd8ddf217e5
Rob Bridges <Rmb101187@gmail.com>
2023-06-16 20:08:07 -0700

    New gallery handler

---
+++ controllers/galleries.go
@@ -0,0 +1,20 @@
+package controllers
+
+import (
+	"github.com/robbridges/webapp_v2/models"
+	"net/http"
+)
+
+type Galleries struct {
+	Templates struct {
+		new Template
+	}
+	GalleryService *models.GalleryService
+}
+
+func (g Galleries) New(w http.ResponseWriter, r *http.Request) {
+	var data struct {
+		Title string
+	}
+	g.Templates.new.Execute(w, r, data)
+}
