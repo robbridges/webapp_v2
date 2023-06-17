cf0920c2d6614c4787b534d9cd3b3bd00fa1ffb9
Rob Bridges <Rmb101187@gmail.com>
2023-06-17 12:02:29 -0700

    Create gallery handler and routing

--- controllers/galleries.go
+++ controllers/galleries.go
@@ -1,6 +1,8 @@
 package controllers
 
 import (
+	"fmt"
+	"github.com/robbridges/webapp_v2/context"
 	"github.com/robbridges/webapp_v2/models"
 	"net/http"
 )
@@ -18,4 +20,24 @@ func (g Galleries) New(w http.ResponseWriter, r *http.Request) {
 	}
 	data.Title = r.FormValue("title")
 	g.Templates.New.Execute(w, r, data)
+}
+
+func (g Galleries) Create(w http.ResponseWriter, r *http.Request) {
+	logger := r.Context().Value("logger").(models.LogInterface)
+
+	var data struct {
+		UserID int
+		Title  string
+	}
+
+	data.UserID = context.User(r.Context()).ID
+	data.Title = r.FormValue("title")
+	gallery, err := g.GalleryService.Create(data.Title, data.UserID)
+	if err != nil {
+		logger.Create(err)
+		g.Templates.New.Execute(w, r, data, err)
+	}
+	//TODO implement this page
+	editPath := fmt.Sprintf("/galleries/%d/edit", gallery.ID)
+	http.Redirect(w, r, editPath, http.StatusFound)
 }
