f7d319fac9c5144bbd1f04590862ea5c31986440
Rob Bridges <Rmb101187@gmail.com>
2023-06-23 17:22:10 -0700

    Check content type and extensions of uploaded files

--- models/errors.go
+++ models/errors.go
@@ -1,8 +1,52 @@
 package models
 
-import "errors"
+import (
+	"errors"
+	"fmt"
+	"io"
+	"net/http"
+	"path/filepath"
+)
 
 var (
 	ErrEmailTaken = errors.New("models: email address already in use")
 	ErrNoData     = errors.New("create sql: no rows in result set")
 )
+
+type FileError struct {
+	Issue string
+}
+
+func (fe FileError) Error() string {
+	return fmt.Sprintf("invalid file: %v", fe.Issue)
+}
+
+func CheckContentType(r io.ReadSeeker, allowedTypes []string) error {
+	testBytes := make([]byte, 512)
+	_, err := r.Read(testBytes)
+	if err != nil {
+		return fmt.Errorf("checking content type: %v", err)
+	}
+	_, err = r.Seek(0, 0)
+	if err != nil {
+		return fmt.Errorf("checking content type: %v", err)
+	}
+	contentTypes := http.DetectContentType(testBytes)
+	for _, t := range allowedTypes {
+		if contentTypes == t {
+			return nil
+		}
+	}
+	return FileError{
+		Issue: fmt.Sprintf("invalid content type"),
+	}
+}
+
+func checkExtension(filename string, allowedExtensions []string) error {
+	if hasExtension(filename, allowedExtensions) {
+		return nil
+	}
+	return FileError{
+		Issue: fmt.Sprintf("invalid extension: %v", filepath.Ext(filename)),
+	}
+}
